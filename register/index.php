2a6bff52efc7cf7e91b42685e7d4241b1d9c0c2a
Parasiteneet <wumabeatboxer@yahoo.co.jp>
2021-01-06 03:24:47 +0900

    problems solved!

--- register/index.php
+++ register/index.php
@@ -1,19 +1,20 @@
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Reiko reservation</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
 <header>
   <div class="header-warapper">
-    <img src="Applications/MAMP/htdocs/reiko/img/bedbug-silhouette.jpg" alt="">
+    <img id="main-pic" src="../img/bedbug-silhouette.jpg" alt="">
   </div>
 </header>
-  
+  <h1>aaaa</h1>
 
-  <script src="js/main.js"></script>
+  <script src="../js/main.js"></script>
 </body>
 </html>
