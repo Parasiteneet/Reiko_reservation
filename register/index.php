725546e4ab2d8c88265fe03f91aa466c2306e3d2
Parasiteneet <wumabeatboxer@yahoo.co.jp>
2021-01-08 23:45:26 +0900

    made a page of reservation

--- register/index.php
+++ register/index.php
@@ -7,26 +7,25 @@
   <title>Reiko reservation</title>
   <link rel="stylesheet" href="../css/style.css">
 </head>
+<link rel="stylesheet" href="../css/responsive.css">
 <body>
 <header>
   <div class="header-warapper">
-    <img id="main-pic" src="../img/bedbug-silhouette.jpg" alt="">
-    <p>貝沼 麗子のご予約サイトへようこそ。</p>
-    <p>My passion is ~~~</p>
-    <p>I serve hospitality ~~~</p>
-    <p>You can be consulted whenever you'd like to</p>
+    <div class="main-pic">
+      <img id="main-pic" src="../img/bedbug-silhouette.jpg" alt="">
+    </div>
+    <div class="main-prof">
+      <p>貝沼 麗子のご予約サイトへようこそ。</p>
+      <p>My passion is ~~~</p>
+      <p>I serve hospitality ~~~</p>
+      <p>You can be consulted whenever you'd like to</p>
+    </div>
   </div>
 </header>
 <main>
-  <div class="register">
-    <form action="check.php" method="post">
-      <div class="centerform">
-        名前：<input type="text" name="name" placeholder="名前" class="name">
-        メールアドレス：<input type="text" name="email"  placeholder="メールアドレス" class="email">
-        パスワード：<input type="password" name="pin"  placeholder="パスワード" class="pin">
-        <div id="submit"><input type="submit" value="入力内容を確認する" class="sub"></div>            
-      </div>
-    </form>
+  <div class="reserve-link">
+    <p>会員登録は<span class="back-color"><a href="reserve.php">コチラ</a></span>からお願いします。</p>
+    <p>すでに登録がお済みの方は<span class="back-color"><a href="../login/login.php">コチラ</a></span>からログインをお願いします。</p>
   </div>
 </main>
 
