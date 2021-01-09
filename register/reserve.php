524e180ae7a22e9e391b188342037ab1095c886c
Parasiteneet <wumabeatboxer@yahoo.co.jp>
2021-01-10 01:46:26 +0900

    add css and php

--- register/reserve.php
+++ register/reserve.php
@@ -1,5 +1,19 @@
 <?php
   require('../app/dbconnect.php');
+
+
+  if ($_POST['name'] === "") {
+    $error['name'] = 'blank';
+  }
+  if ($_POST['email'] === "") {
+    $error['email'] = 'blank';
+  }
+
+  if ($_POST['pin'] === "") {
+    $error['pin'] = 'blank';
+  }
+
+
 ?>
 
 <!DOCTYPE html>
@@ -7,18 +21,36 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>予約フォーム</title>
   <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="../css/responsive.css">
 </head>
 <body>
 <div class="register">
-  <form action="check.php" method="post">
-    <div class="centerform">
-      名前：<input type="text" name="name" size="35" maxlength="255" placeholder="名前" class="name">
-      メールアドレス：<input type="text" name="email" size="35" maxlength="255" placeholder="メールアドレス" class="email">
-      パスワード：<input type="password" name="pin"  size="10" maxlength="20" placeholder="パスワード" class="pin">
-      <div id="submit"><input type="submit" value="入力内容を確認する" class="sub"></div>
+  <form action="" method="post">
+    <div class="container">
+      <div class="item">
+          名前：<input type="text" name="name" size="35" maxlength="255" placeholder="名前" class="name" value="<?php echo htmlspecialchars($_POST['name'], ENT_QUOTES)?>"> 
+            <?php if ($error['name'] === 'blank'): ?>
+              <script> alert('名前を入力してください'); </script>
+            <?php endif; ?>
+      </div>
+      <div class="item">
+          メールアドレス：<input type="text" name="email" size="35" maxlength="255" placeholder="メールアドレス" class="email" value="<?php echo htmlspecialchars($_POST['email'], ENT_QUOTES)?>">
+            <?php if ($error['email'] === 'blank'): ?>
+              <script> alert('メールアドレスを入力してください'); </script>
+            <?php endif; ?>
+      </div>
+      <div class="item">
+          パスワード：<input type="password" name="pin"  size="10" maxlength="20" placeholder="パスワード" class="pin" value="<?php echo htmlspecialchars($_POST['pin'], ENT_QUOTES)?>">
+            <?php if ($error['pin'] === 'blank'): ?>
+              <script> alert('パスワードを入力してください'); </script>
+            <?php endif; ?>
+      </div>
     </div>
+      <div id="submit">
+        <input type="submit" value="入力内容を確認する" class="sub">
+      </div>
   </form>
 </div>
 
