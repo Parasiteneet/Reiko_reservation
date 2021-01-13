63bdca0c29676da2219cd3f9a22fad4947ecee3f
Parasiteneet <wumabeatboxer@yahoo.co.jp>
2021-01-14 02:19:20 +0900

    completed login system with session && cookie

--- register/index.php
+++ register/index.php
@@ -1,21 +1,43 @@
 <?php 
+session_start();
 require('../app/dbconnect.php');
 
 function h($s) {
   return htmlspecialchars($s, ENT_QUOTES, 'utf-8');
 }
 
-session_start();
-
-
-if (isset($_SESSION['email'])) {
-  echo 'ようこそ'. h($_SESSION['email']) . "さん<br>";
-  echo "<a href='../login/logout.php'>ログアウトはこちら。</a>";
-  exit;
-} else {
-  echo "<script> alert('新規会員登録の方はこのページの下記のリンクから会員登録をお願いします。'); </script>";
+if ($_COOKIE['email'] !== '') {
+  $email = $_COOKIE['email'];
 }
 
+if (!empty($_POST)) {
+  $email = $_POST['email'];
+  
+  if ($_POST['email'] !== '' && $_POST['pin'] !== '') {
+    $login = $db->prepare('SELECT * FROM members WHERE email=? AND pin=?');
+    $login->execute(array(
+      $_POST['email'],
+      sha1($_POST['pin'])
+    ));
+    $member = $login->fetch(); //データが返ってきているかを判断する。
+
+    if ($member) {
+      $_SESSION['id'] = $member['id'];
+      $_SESSION['time'] = time();
+
+      if ($_POST['save'] ==='on') {
+        setcookie('email', $_POST['email'], time() + (20 * 365 * 24 * 60 * 60));
+      }
+
+      header('Location: ../register/thanks.php');
+      exit;
+    } else {
+      $error['login'] = 'failed';
+    }
+  } else {
+    $error['login'] = 'blank';
+ }
+}
 
 ?>
 
@@ -40,24 +62,40 @@ if (isset($_SESSION['email'])) {
       <p>I serve you hospitality ~~~</p>
       <p>You can be consulted whenever you'd like to</p>
     </div>
+  <div class="error-box">
+    <?php if ($error['login'] === 'blank'): ?>
+      <p id="error-msg">※正しいメールアドレスとパスワードを入力してください</p>
+    <?php endif; ?>
+    <?php if ($error['login'] === 'failed'): ?>
+      <p id="error-msg">※ログインに失敗しました。正しくご記入ください</p>
+    <?php endif; ?>
+  </div>
   </div>
 </header>
+
 <main>
   <div class="login-container">
-    <form action="">
+    <form action="" method="post">
       <label for="index-email" class="label1">メールアドレス</label>      
       <label for="index-pin" class="label2">パスワード</label> 
       <br>     
-        <input type="email" name="email" id="index-email">
-        <input type="password" name="pin" id="index-pin">
+        <input type="email" name="email" id="index-email"
+        value="<?php echo h($email); ?>">
+        <input type="password" name="pin" id="index-pin"
+        value="<?php echo h($_POST['pin']); ?>">
       </br>
       <br>
         <button type="submit" id="index-login">ログイン</button>
       </br>
+      <div class="auto-login">
+      <input type="checkbox" id="save" name="save" value="on">
+      <label for="save">次回から自動的にログインする</label>
+      </div>
+
     </form>
   </div>
   <div class="register-link">
-    <p>新規会員登録は<span class="back-color"><a href="reserve.php">コチラ</a></span>からお願いします。</p>
+    <p>新規会員登録は<span class="back-color"><a href="register.php">コチラ</a></span>からお願いします。</p>
     <!-- <p>すでに登録がお済みの方は<span class="back-color"><a href="../login/login.php">コチラ</a></span>からログインをお願いします。</p> -->
   </div>
 </main>
